b612278918d3e90f0ed8debf93a94b219280dd01
bigbabyplus <82078694+bigbabyplus@users.noreply.github.com>
2021-05-07 19:31:12 +0800

    Update huawei.py

--- clients/huawei.py
+++ clients/huawei.py
@@ -62,16 +62,16 @@ class HuaWei(BaseHuaWei):
         await asyncio.sleep(5)
      
 
-       # await self.page.click('.textBtn')
-       # await asyncio.sleep(20)
+        await self.page.click('.textBtn')
+        await asyncio.sleep(20)
 
-       # await self.page.type('input[class="hwid-input"]','797881',{'delay': 10})
-       # await asyncio.sleep(5)
+        await self.page.type('input[class="hwid-input"]','797881',{'delay': 10})
+        await asyncio.sleep(5)
 
-       # await self.page.click('.dialog-btn.dialog-btn.btn-next.hwid-btn-primary')
-       # await asyncio.sleep(5)
-       # await self.page.click('.dialog-btn.dialog-btn.btn-next.hwid-btn-primary.hwid-btn-primary')
-       # await asyncio.sleep(5)
+        await self.page.click('.dialog-btn.dialog-btn.btn-next.hwid-btn-primary')
+        await asyncio.sleep(5)
+        await self.page.click('.dialog-btn.dialog-btn.btn-next.hwid-btn-primary.hwid-btn-primary')
+        await asyncio.sleep(5)
 
     async def iam_login(self, username, password, parent):
         self.parent_user = os.environ.get('PARENT_USER', parent)

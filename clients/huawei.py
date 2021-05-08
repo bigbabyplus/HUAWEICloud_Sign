821def63f6f188ff5bb734f07d5dca97ccadbe73
bigbabyplus <82078694+bigbabyplus@users.noreply.github.com>
2021-05-08 09:44:07 +0800

    Update huawei.py

--- clients/huawei.py
+++ clients/huawei.py
@@ -66,7 +66,7 @@ class HuaWei(BaseHuaWei):
         # await self.logger.info(await self.page.content())
         await self.page.click('.textBtn')
         await asyncio.sleep(20)
-        await self.page.type('input[class="hwid-input"]','877435',{'delay': 10})
+        await self.page.type('input[class="hwid-input"]','847984',{'delay': 10})
         await asyncio.sleep(5)
         await self.page.click('.dialog-btn.dialog-btn.btn-next.hwid-btn-primary')
         await asyncio.sleep(5)

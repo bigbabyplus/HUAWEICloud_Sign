f920b54e61fa4136d0dd9972fec2d2d257d6d3bf
bigbabyplus <82078694+bigbabyplus@users.noreply.github.com>
2021-05-08 11:19:18 +0800

    Update huawei.py

--- clients/huawei.py
+++ clients/huawei.py
@@ -64,14 +64,14 @@ class HuaWei(BaseHuaWei):
         #await self.page.click('.dialog-btn.dialog-btn.btn-next.hwid-btn-primary')
         #await asyncio.sleep(5)
         #self.logger.info(await self.page.content())
-        #await self.page.click('.textBtn')
-        #await asyncio.sleep(20)
-        #await self.page.type('input[class="hwid-input"]','744229',{'delay': 10})
-        #await asyncio.sleep(5)
-        #await self.page.click('.dialog-btn.dialog-btn.btn-next.hwid-btn-primary')
-        #await asyncio.sleep(5)
-        #await self.page.click('.dialog-btn.dialog-btn.btn-next.hwid-btn-primary.hwid-btn-primary')
-        #await asyncio.sleep(5)
+        await self.page.click('.textBtn')
+        await asyncio.sleep(20)
+        await self.page.type('input[class="hwid-input"]','744229',{'delay': 10})
+        await asyncio.sleep(5)
+        await self.page.click('.dialog-btn.dialog-btn.btn-next.hwid-btn-primary')
+        await asyncio.sleep(5)
+        await self.page.click('.dialog-btn.dialog-btn.btn-next.hwid-btn-primary.hwid-btn-primary')
+        await asyncio.sleep(5)
 
         
     async def iam_login(self, username, password, parent):
